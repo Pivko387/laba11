e01ef0180cc2bb79353bb45582c5aaf3fc6b49e2
Никита Черничкин <nikita.macarov@yandex.ru>
2022-11-30 15:34:30 +0300

    Добавьте файлы проекта.

---
+++ laba11/Classes.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace laba11
+{
+    public enum gender { Male, Female }
+    public enum user_type { Employee, Guest, Abiturient}
+    public class Subject
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        Subject(string name,int score)
+        {
+            if (score > 100 || score < 1)
+                throw new ArgumentException("Неверно введено значение балла!");
+            Score = score;
+            Name = name;
+        }
+    }
+    public abstract class User
+    {
+        public string Password { get; set; }
+        public string Login { get; set; }
+        public string AuthCode { get; set; }
+        public virtual void Auth()
+        {
+        }
+    }
+    public class Person : User
+    {
+        public user_type Type { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronim { get; set; }
+        public gender Gender { get; set; }
+        public DateTime Birth { get; set; }
+        public Person() { }
+        public Person(string name, string lastname, string patronim, short gendr,DateTime brth)
+        {
+            Name = name;
+            Surname = lastname;
+            Patronim = patronim;
+            Birth = brth;
+            if (gendr == 0) Gender = gender.Male;
+            if (gendr == 1) Gender = gender.Female;
+        }
+        public override void Auth()
+        {
+            string path = (@"\lab10\users\" + Login + ".acc");
+            if (File.Exists(path))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                var user = new Employee();
+                using (System.IO.FileStream fs = new System.IO.FileStream(path, System.IO.FileMode.OpenOrCreate)) //bin
+                {
+                    try { user = formatter.Deserialize(fs) as Employee; }
+                    catch (Exception ex) { }
+                    fs.Close();
+                }
+                if (Password == user.Password)
+                {
+                    //base_form form = new base_form(user);
+                    //form.Show();
+                }
+                else throw new Exception("Неверный логин или пароль!");
+            }
+            else throw new Exception("Неверный логин или пароль!");
+        }
+    }
+    [Serializable]
+    public class Employee : Person
+    {
+        public short FacultyID { get; set; }
+        public short Worker { get; set; }
+        public Employee(string Log, string pass, string name, string lastname, string patronim, short gendr, short facul, DateTime brth, short work)
+        {
+            Type = user_type.Employee;
+            Name = name;
+            Surname = lastname;
+            Patronim = patronim;
+            Login = Log;
+            Password = pass;
+            Birth = brth;
+            if (gendr == 0) Gender = gender.Male;
+            if (gendr == 1) Gender = gender.Female;
+            FacultyID = facul;
+            Worker = work;
+            Random rnd = new Random();
+            AuthCode = rnd.Next(1000, 9999).ToString() + "-" + rnd.Next(1000, 9999).ToString() + "-" + rnd.Next(1000, 9999).ToString()
+                + "-" + rnd.Next(1000, 9999).ToString();
+        }
+        public Employee() 
+        {
+            Type = user_type.Employee;
+        }
+        public override void Auth()
+        {
+            string path = (@"\lab10\users\" + Login + ".acc");
+            if (File.Exists(path))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                var user = new Employee();
+                using (System.IO.FileStream fs = new System.IO.FileStream(path, System.IO.FileMode.OpenOrCreate)) //bin
+                {
+                    try { user = formatter.Deserialize(fs) as Employee; }
+                    catch { }
+                    fs.Close();
+                }
+                if (Password == user.Password)
+                {
+                    //base_form form = new base_form(user);
+                    //form.Show();
+                }
+                else throw new Exception("Неверный логин или пароль!");
+            }
+            else throw new Exception("Неверный логин или пароль!");
+        }
+    }
+    public class Abiturient : Person
+    {
+        private List<Subject> subjects { get; }
+        private List<short> directions { get; }
+        public Abiturient() 
+        {
+            subjects = new List<Subject>();
+            directions = new List<short>();
+            Type = user_type.Abiturient;
+        }
+        public Abiturient(string Log, string pass, string name, string lastname, string patronim, short gendr, DateTime brth)
+        {
+            subjects = new List<Subject>();
+            directions = new List<short>();
+            Type = user_type.Abiturient;
+
+            Name = name;
+            Surname = lastname;
+            Patronim = patronim;
+            Login = Log;
+            Password = pass;
+            Birth = brth;
+            if (gendr == 0) Gender = gender.Male;
+            if (gendr == 1) Gender = gender.Female;
+            Random rnd = new Random();
+            AuthCode = rnd.Next(1000, 9999).ToString() + "-" + rnd.Next(1000, 9999).ToString() + "-" + rnd.Next(1000, 9999).ToString()
+                + "-" + rnd.Next(1000, 9999).ToString();
+        }
+        public void AddSubject(Subject sub)
+        {
+            if (subjects.Contains(sub) == false)
+                subjects.Add(sub);
+            else
+                throw new Exception("Данный предмет уже добавлен!");
+        }
+        public void AddDirection(short id)
+        {
+            if(directions.Count < 5)
+                if(directions.Contains(id)==false)
+                    directions.Add(id);
+                else
+                    throw new Exception("Данное направление уже добавлено!");
+            else
+                throw new Exception("Невозможно выбрать более 5 направлений!");
+        }
+        public void RemoveSubject(int id)
+        {
+            if(id >= 0 && id < subjects.Count)
+            {
+                subjects.RemoveAt(id);
+            }
+            else throw new Exception("Неверно указан идентификатор предмета для удаления!");
+        }
+        public void RemoveDirection(int id)
+        {
+            if (id >= 0 && id < directions.Count)
+            {
+                directions.RemoveAt(id);
+            }
+            else throw new Exception("Неверно указан идентификатор направления для удаления!");
+        }
+        public override void Auth()
+        {
+            string path = (@"\lab10\users\" + Login + ".acc");
+            if (File.Exists(path))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                var user = new Abiturient();
+                using (System.IO.FileStream fs = new System.IO.FileStream(path, System.IO.FileMode.OpenOrCreate)) //bin
+                {
+                    try { user = formatter.Deserialize(fs) as Abiturient; }
+                    catch {}
+                    fs.Close();
+                }
+                if (Password == user.Password)
+                {
+                    //base_form form = new base_form(user);
+                    //form.Show();
+                }
+                else throw new Exception("Неверный логин или пароль!");
+            }
+            else throw new Exception("Неверный логин или пароль!");
+        }
+    }
+}
---
+++ laba11/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace laba11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.showPass = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.register_butt = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pass_box = new System.Windows.Forms.TextBox();
+            this.login_box = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(13, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(272, 25);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Войти через файл ключа";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // showPass
+            // 
+            this.showPass.AutoSize = true;
+            this.showPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.showPass.Location = new System.Drawing.Point(14, 188);
+            this.showPass.Name = "showPass";
+            this.showPass.Size = new System.Drawing.Size(160, 24);
+            this.showPass.TabIndex = 14;
+            this.showPass.Text = "Показать пароль";
+            this.showPass.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(108, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Пароль";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(69, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Имя пользователя";
+            // 
+            // register_butt
+            // 
+            this.register_butt.AutoSize = true;
+            this.register_butt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.register_butt.Location = new System.Drawing.Point(15, 327);
+            this.register_butt.Name = "register_butt";
+            this.register_butt.Size = new System.Drawing.Size(270, 20);
+            this.register_butt.TabIndex = 11;
+            this.register_butt.Text = "Нет учётной записи? Регистрация";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(13, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(272, 38);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Авторизация";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pass_box
+            // 
+            this.pass_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pass_box.Location = new System.Drawing.Point(13, 143);
+            this.pass_box.Name = "pass_box";
+            this.pass_box.PasswordChar = '*';
+            this.pass_box.Size = new System.Drawing.Size(272, 38);
+            this.pass_box.TabIndex = 9;
+            // 
+            // login_box
+            // 
+            this.login_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.login_box.Location = new System.Drawing.Point(13, 82);
+            this.login_box.Name = "login_box";
+            this.login_box.Size = new System.Drawing.Size(272, 38);
+            this.login_box.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(13, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(272, 25);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Войти как гость";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Преподаватель",
+            "Абитуриент"});
+            this.comboBox1.Location = new System.Drawing.Point(112, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 28);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Войти как: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 355);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.showPass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.register_butt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pass_box);
+            this.Controls.Add(this.login_box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox showPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label register_butt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox pass_box;
+        private System.Windows.Forms.TextBox login_box;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
